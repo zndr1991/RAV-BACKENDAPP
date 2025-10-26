33b41cfa16f3b7666ca4c77098ab5e10cecc6244
zenhder <zenhders@gmail.com>
2025-10-26 14:26:41 -0600

    Forzar uso de IPv4 en conexiones PG

--- db.js
+++ db.js
@@ -3,7 +3,11 @@ const dns = require('dns');
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Prioritize IPv4 to avoid ENETUNREACH errors in environments without IPv6 routing
+const lookupIPv4 = (hostname, options, callback) => {
+  const lookupOptions = { ...options, family: 4, hints: dns.ADDRCONFIG };
+  return dns.lookup(hostname, lookupOptions, callback);
+};
+
 if (typeof dns.setDefaultResultOrder === 'function') {
   dns.setDefaultResultOrder('ipv4first');
 }
@@ -12,7 +16,13 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  keepAlive: true,
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
+  max: 10,
+  allowExitOnIdle: true,
+  lookup: lookupIPv4
 });
 
 module.exports = pool;
