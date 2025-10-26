93daf652d5e302e7faea4d5b01a1df575242201e
zenhder <zenhders@gmail.com>
2025-10-26 14:18:58 -0600

    Forzar IPv4 en conexiones PG

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 
+const dns = require('dns');
 const { Pool } = require('pg');
 require('dotenv').config();
+
+// Prioritize IPv4 to avoid ENETUNREACH errors in environments without IPv6 routing
+if (typeof dns.setDefaultResultOrder === 'function') {
+  dns.setDefaultResultOrder('ipv4first');
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
